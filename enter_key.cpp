fabb8e600b4391c24621fa207481e6c2bc333318
piyushkrana1 <42732203+piyushkrana1@users.noreply.github.com>
2022-08-30 21:42:24 +0530

    Add files via upload

--- enter_key.cpp
+++ enter_key.cpp
@@ -12,13 +12,15 @@
 #include <grp.h>
 using namespace std;
 int x,y,fsize;
-char *curr_dir=NULL;
-vector<string>files;
 
+char *curr_dir=NULL;
+// vector<string>files;
+vector<pair<string,string>>files;
+pair<string,string>p;
 void cursor_print(int x,int y){
     // printf("\033[",y,x);
     cout<<"\033["<<y<<";"<<x<<"H";
-    fflush(stdout);
+    // fflush(stdout);
 }
 
 
@@ -37,18 +39,21 @@ int printdata(const char *curr_dir){
     dp=opendir((const char*)curr_dir);
     if(dp==NULL){return -1;}
     for(auto count=0;(dptr=readdir(dp))!=NULL;count++){
-        files.push_back(dptr->d_name);
+        p.first=dptr->d_name;
+        p.second="";
+        files.push_back(p);
     }
     // cout<<endl;
     sort(files.begin(),files.end());
     dp=opendir((const char*)curr_dir);
-
     string s;
-
     for(auto c=0;(dptr=readdir(dp))!=NULL;c++){
-        s=(string)curr_dir+"/"+files[c];
+        s=(string)curr_dir+"/"+files[c].first;
         stat(s.c_str(),&fileStat);
         printf((S_ISDIR(fileStat.st_mode))?"d" :"-");
+        if((S_ISDIR(fileStat.st_mode))){
+            files[c].second="d";
+        }
         printf( (fileStat.st_mode&S_IRUSR)?"r" :"-");
         printf( (fileStat.st_mode&S_IWUSR)?"w" :"-");
         printf( (fileStat.st_mode&S_IXUSR)?"x" :"-");
@@ -57,17 +62,21 @@ int printdata(const char *curr_dir){
         printf( (fileStat.st_mode&S_IXGRP)?"x" :"-");
         printf( (fileStat.st_mode&S_IROTH)?"r" :"-");
         printf( (fileStat.st_mode&S_IWOTH)?"w" :"-");
-        printf( (fileStat.st_mode&S_IXUSR)?"x" :"-");
+        printf( (fileStat.st_mode&S_IXOTH)?"x" :"-");
         pw=getpwuid(fileStat.st_uid);
         printf("\t%s",pw->pw_name);
         gp=getgrgid(fileStat.st_gid);
         printf("\t%s",gp->gr_name);
+        // // stat(files[c].c_str(),&fileStat);
         printf("\t %.2fK\t",(float)fileStat.st_size/1024);
         ct=ctime(&fileStat.st_mtime);
         for(int i=4;i<=15;i++){
             printf("%c",ct[i]);
         }
-        printf("\t %s\n",files[c].c_str());
+
+        printf("\t %s\n",files[c].first.c_str());
+
+        // printf("\n");
         fsize=files.size();
 }
 return 0;
@@ -75,7 +84,7 @@ return 0;
 
 struct termios original_param; 
 void disable_normal_mode(){
-tcsetattr(STDIN_FILENO,TCSAFLUSH,&original_param);
+    tcsetattr(STDIN_FILENO,TCSAFLUSH,&original_param);
 }
 
 void enable_normal_mode(){
@@ -88,26 +97,54 @@ void enable_normal_mode(){
     // normal_param.c_lflag&=~(OPOST);
     tcsetattr(STDIN_FILENO,TCSAFLUSH,&normal_param);
 }
+
 void down_cursor(){
     if(y<fsize){
     cursor_print(x,++y);
     }
 }
+
 void up_cursor(){
     if(y>0){
     cursor_print(x,--y);
     }
 }
+
 void clean(){
     printf("\033[H\033[2J\033[3J");
 }
 
 void enter_key(){
+            // cout<<"\t\t\t\t\t\t"<<""
+// //             // cout<<"enter";
             // cout<<"\t\t"<<files[y-1];
             curr_dir=getenv("PWD");
-            if(files[y-1]!="."){
+            // struct stat fstattemp;
+            // string s=(string)curr_dir+"/"+files[y-1];
+            // cout<<s;
+            // stat(files[y-1].c_str(),&fstattemp);
+            // if(!S_ISDIR(fstattemp.st_mode)){
+            //     execl("/bin/sh", "sh", "-c",xdg-open, (char *) NULL);
+            // }
+// //             char cwd[256];
+// //             getcwd(cwd,sizeof(cwd));
+
+            //For files
+            if(files[y-1].first!="."&&files[y-1].second!="d"){
+                string s=(string)curr_dir+"/"+files[y-1].first;
+                pid_t baby=fork();
+                if(baby==0){
+                execl("/bin/sh", "sh", "-c",("xdg-open "+s).c_str(), (char *) NULL);
+                }
+            }
+
+
+
+            //for directories
+            if(files[y-1].first!="."&&files[y-1].second=="d"){
             curr_dir=strcat(curr_dir,"/");
-            curr_dir=strcat(curr_dir,files[y-1].c_str());
+            curr_dir=strcat(curr_dir,files[y-1].first.c_str());
+//         //    cout<<"\t\t"<<curr_dir;
             clean();
             files.clear();
             printdata(curr_dir);
@@ -117,12 +154,14 @@ void enter_key(){
 }
 
 
+
 int main()
 {   x=1,y=1;
     curr_dir=getenv("PWD");
     clean();
     enable_normal_mode();
     printdata(curr_dir);
+    // files.clear();
     cursor_print(x,y);
     char ch;
     while(ch=getchar()){
